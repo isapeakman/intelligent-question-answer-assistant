36f06900dfbb25c7ab02f2d4bf9f5e00737ebc3e
peak-like <2095991530@qq.com>
2024-11-25 10:11:05 +0800

    init project structure

---
+++ src/test/java/com/lightcs/IntelligentQuestionAnswerAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentQuestionAnswerAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
